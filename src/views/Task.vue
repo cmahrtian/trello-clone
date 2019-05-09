fd1ea5b33df065c2c65304de543c9ef1b46748bf
Jennifer Paulino <jenpaulino3@gmail.com>
2019-05-09 17:29:16 -0400

    added task modal created compute property task () to return the task matching the provided id

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,13 +1,31 @@
 <template>
     <div class="task-view">
+        <div class="task-modal">
+            {{ task.name }}
+        </div>
     </div>
 </template>
 
 <script>
-export default {
+import { mapGetters } from 'vuex'
 
+export default {
+  computed: {
+    ...mapGetters([
+      'getTask'
+    ]),
+    task () {
+      return this.getTask(this.$route.params.id)
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    .task-modal {
+        background-color: #fff;
+        padding: 15px;
+        border-radius: 5px;
+        width: 50%;
+    }
 </style>
